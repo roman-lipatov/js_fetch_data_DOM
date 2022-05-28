75630003e6fea2daa897c93616e35f66ef3321ba
Roman Lipatov <96875973+roman-lipatov@users.noreply.github.com>
2022-05-28 13:28:01 +0300

    fetch-data

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,46 @@
 'use strict';
 
 // write your code here
+const BASE = 'https://mate-academy.github.io/'
+    + 'phone-catalogue-static/api/phones.json';
+const url = 'https://mate-academy.github.io/'
++ 'phone-catalogue-static/api/phones/';
+const body = document.querySelector('body');
+const ul = document.createElement('ul');
+
+body.append(ul);
+
+const getPhones = () => {
+  fetch(BASE)
+    .then(response => {
+      setTimeout(() => {
+        if (!response.ok) {
+          throw new Error('Fetch failed');
+        }
+      }, 5000);
+
+      return response.json();
+    }).then(phonesList => {
+      getPhonesDetails(phonesList);
+    });
+};
+
+const getPhonesDetails = (data) => {
+  for (const el of data) {
+    fetch(url + el.id + '.json')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error(`Failed to fetch ${el.id} details`);
+        }
+
+        return response.json();
+      }).then(res => {
+        const li = document.createElement('li');
+
+        li.innerText = res.name;
+        ul.append(li);
+      });
+  }
+};
+
+getPhones();
